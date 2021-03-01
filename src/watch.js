dcd99d2abd800b20ae66789ab5476841ee6d9bc4
karen9999 <karen.dev999@gmail.com>
2021-03-01 21:19:42 +0300

    add watcher

---
+++ src/watch.js
@@ -0,0 +1,24 @@
+import onChange from 'on-change';
+import { renderError, renderContent, highlightForm } from './view';
+
+export default (state) => onChange(state, (path, value) => {
+  switch (path) {
+    case 'submitForm.state': {
+      highlightForm(value);
+      break;
+    }
+    case 'submitForm.validationErrors': {
+      renderError(value);
+      break;
+    }
+    case 'submitForm.errors': {
+      renderError(value);
+      console.log(state)
+      break;
+    }
+    case 'data.posts': {
+      renderContent(state.data.feeds, state.data.posts);
+      break;
+    }
+  }
+})
